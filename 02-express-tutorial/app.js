395ebfa23ab722a70a96d06d433e8b3f4c445132
Chandan Pavan <chandu.h2024@gmail.com>
2026-01-08 18:22:01 +0530

    last-final-commit

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,27 +1,42 @@
-const http = require("http");
-const { readFileSync } = require("fs");
+const express = require("express");
+const app = express();
+const { products } = require("./data.js");
 
-//get all files
-const homePage = readFileSync("./index.html");
-
-const server = http.createServer((req, res) => {
-  //   console.log("user has hit the server");
-  //   res.end("Home page");
-  //  console.log(req.method);
-  const url = req.url;
-  if (url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>Home page<h1>");
-    res.end();
-  } else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>About page<h1>");
-    res.end();
-  } else {
-    res.writeHead(404, { "content-type": "text/html" });
-    res.write("<h1>Page not found<h1>");
-    res.end();
-  }
+app.get("/", (req, res) => {
+  res.send('<h1>Home page<h1><a href="/api/products">products</a>');
 });
 
-server.listen(4000);
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.send(newProducts);
+});
+app.get("/api/products/:productID", (req, res) => {
+  const { productID } = req.params;
+
+  const singleProduct = product.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    return res.status(404).send("product does not exist");
+  }
+  res.json(singleProduct); // in last a return statement is added why is that
+});
+
+app.get("/api/v1/query", (req, res) => {
+  // console.log(req.query);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((products) => {
+      return products.name.startsWith(search);
+    });
+  }
+  res.send("Hello world");
+});
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000");
+});
---
+++ data.js
@@ -0,0 +1,36 @@
+const products = [
+  {
+    id: 1,
+    name: "albony sofa",
+    image: "url comes here",
+    price: 39.5,
+    desc: "test case",
+  },
+  {
+    id: 2,
+    name: "dining table",
+    image: "url comes here",
+    price: 39.5,
+    desc: "test case",
+  },
+  {
+    id: 3,
+    name: "coffee stand",
+    image: "url comes here",
+    price: 39.5,
+    desc: "test case",
+  },
+  {
+    id: 4,
+    name: "furnitures",
+    image: "url comes here",
+    price: 39.5,
+    desc: "test case",
+  },
+];
+const people = [
+  { id: 1, name: "john" },
+  { id: 2, name: "peter" },
+  { id: 3, name: "susan" },
+];
+module.exports = { products, people };
